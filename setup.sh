526389cbbc03d71953d57cd5b3c84fbac37cb73a
Manollo-36 <Varsakise@gmail.com>
2025-01-15 11:54:40 +0200

    Added auth0 aplication details

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 export DATABASE_URL="postgres://ua43dg2ka0ippf:pa0fe848b19ea68dfdadbd82b7bf70504dadfae0f91d9ea8c054cfd541c816911@cc0gj7hsrh0ht8.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d52996bqd5tsd7"                     
 export EXCITED="true"
-export AUTH0_DOMAIN = 'fsndmanos.eu.auth0.com'
-export ALGORITHMS = ['RS256']
-export API_AUDIENCE =  'Capstone'
+export AUTH0_DOMAIN = "fsndmanos.eu.auth0.com"
+export ALGORITHMS = ["RS256"]
+export API_AUDIENCE =  "Capstone"
+export CLIENT_ID= "SzfWEGWwdqVWTYfUxYsXmPTqdQNa6VsM"
+export CLIENT_SECRET = "_zLR05j5uLdrR5521NWXQwnOsESlrcSd3nwWrJ7g_PlIifo5_vuF36ZEplY3m0jJ"
 echo "setup.sh script executed successfully!"
