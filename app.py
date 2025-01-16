403c839fc565cd58fdb4538ca12e09765336a437
Manollo-36 <Varsakise@gmail.com>
2025-01-16 11:48:31 +0200

    Removed login

--- app.py
+++ app.py
@@ -1,73 +1,23 @@
 from os import environ as env
 #from urllib import response
-from flask import Flask,jsonify,request,redirect, url_for,session,render_template
+from flask import Flask,jsonify,request
 from flask_cors import CORS
-import json
 from models import setup_db, Actor,Movie
 from Authentication.auth import AuthError, requires_auth
 from dotenv import find_dotenv, load_dotenv
-from authlib.integrations.flask_client import OAuth
-from urllib.parse import quote_plus, urlencode
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-API_AUDIENCE = env.get("API_AUDIENCE")
-CLIENT_ID = env.get("CLIENT_ID")
-CALLBACK_URI = env.get("CALLBACK_URI")
-
 def create_app(test_config=None):
     app = Flask(__name__)
     app.secret_key = env.get("APP_SECRET_KEY")
+    
     with app.app_context():
         setup_db(app)
-        CORS(app)
-        oauth = OAuth(app)
-
-    oauth.register(
-        "auth0",
-        client_id=env.get("AUTH0_CLIENT_ID"),
-        client_secret=env.get("AUTH0_CLIENT_SECRET"),
-        client_kwargs={
-            "response_type": "token",
-            "scope": "openid profile email"
-        },
-        server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
-    )
-
-    @app.route("/login")
-    def login():
-        return oauth.auth0.authorize_redirect(
-            redirect_uri=url_for("callback", _external=True)
-        )
+        CORS(app)       
     
-    @app.route("/callback", methods=["GET", "POST"])
-    def callback():
-        token = oauth.auth0.authorize_access_token()
-        session["user"] = token
-        return redirect("/")    
-    
-
-    @app.route("/")
-    def home():
-        return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
-    
-    @app.route("/logout")
-    def logout():
-        session.clear()
-        return redirect(
-            "https://" + env.get("AUTH0_DOMAIN")
-            + "/v2/logout?"
-            + urlencode(
-                {
-                    "returnTo": url_for("home", _external=True),
-                    "client_id": env.get("AUTH0_CLIENT_ID"),
-                },
-                quote_via=quote_plus,
-            )
-        )
-
     @app.route('/actors',methods=['GET'])
     @requires_auth('get:actors')
     def get_actors(self):
@@ -311,4 +261,4 @@ def create_app(test_config=None):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="localhost",debug=True,port="8100")
